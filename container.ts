c102289fa8960ec8854cc434094f515a52462242
hydromoll <hydromoll@mail.ru>
2023-11-05 18:06:18 +0300

    add keyboard dissmiss to container

--- container.ts
+++ container.ts
@@ -1,12 +1,19 @@
+import { Keyboard} from "react-native";
 import styled from "styled-components/native";
 
 interface Props {
   ph?: number;
   bg?: string;
 }
+const keyboardDismiss = () => {
+  Keyboard.dismiss();
+  return false;
+};
+
 
 export const Container = styled.View.attrs(({ ph }: Props) => ({
   paddingHorizontal: ph,
+  onStartShouldSetResponder: keyboardDismiss,
 }))<Props>`
   width: 100%;
   height: 100%;
